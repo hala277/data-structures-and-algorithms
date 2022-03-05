69184dbf2175298d7c4618e9620af0b660b65d8e
hala277 <hala.almasharfeh@gmail.com>
2022-03-05 21:46:12 +0300

    add linked-list

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,73 @@
 'use strict';
 
+
+const { describe, it } = require('eslint/lib/rule-tester/rule-tester');
+// const { it } = require('eslint/lib/rule-tester/rule-tester');
 // Require our linked list implementation
+
 const LinkedLilst = require('../index');
 
-describe('Linked List', () => {
+describe('test Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  it('Can successfully instantiate an empty linked list',() => {
+   const ll = new LinkedLilst();
+  //  expect(ll).toBeInstanceOf(LinkedLilst);
+   expect(ll).toBeDefined();
+   expect(ll.head).toBeNull();
+  })
+
 });
+
+
+describe('The head property will properly point to the first node in the linked list', () => {
+
+  it('Can properly insert into the linked list', () => {
+    const ll = new LinkedLilst(); 
+    ll.insert(1);
+    expect(ll.head.value).toEqual(1);
+    expect(ll.head.next).toBeNull();
+    
+  })
+
+  it('Can properly insert multiple nodes into the linked list', () => {
+   
+    const ll = new LinkedLilst(); 
+    ll.insert(1);
+    ll.insert(2);
+    expect(ll.head.value).toEqual(2);
+    expect(ll.head.next.value).toBe(1);
+    
+  })
+})
+
+describe('return true,false thing', () => {
+it('return true when finding a value within the linked list that exists',() => {
+let ll = new LinkedLilst();
+ll.insert(1);
+ll.insert(2);
+ll.insert(3);
+ll.insert(4);
+expect(ll.includes(4)).toBe(true);
+})
+
+it('return false when searching for a value in the linked list that does not exist',() => {
+  let ll = new LinkedLilst();
+  ll.insert(1);
+  ll.insert(2);
+  ll.insert(3);
+  ll.insert(4);
+  expect(ll.includes(5)).toBe(false);
+})
+
+it('return a collection of all the values that exist in the linked list',() => {
+  let ll = new LinkedLilst();
+  ll.insert('a');
+  ll.insert('b');
+  ll.insert('c');
+  expect(ll.toString()).toBe("{ c }->{ b }->{ a }->NULL");
+})
+
+})
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,51 @@
 'use strict';
 
+const Node = require('./node.js');
 class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(value) {
+        //Adds a new node with that value to the head of the list with an O(1) Time performance.
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        }
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+
+    includes(value) {
+
+        const node = this.head;
+        while (node !== null) {
+            if (node.value == value) {
+                return true
+            }
+            else if (node.value !== value) {
+                return false;
+            }
+
+        }
+
+    }
+
+    toString() {
+        let string = '';
+        while (this.head !== null) {
+            string = string + `{ ${this.head.value} }->`;
+            this.head = this.head.next;
+        }
+
+        string = string + 'NULL';
+        return string;
+    }
 
 }
+
+
 
 module.exports = LinkedList;
